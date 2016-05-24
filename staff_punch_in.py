13d35d108b2f3c0a1166d5b86d5227d8bdff98ff
hanscholem <evilchian.g@gmail.com>
2016-05-25 03:23:08 +0800

    quick update

--- staff_punch_in.py
+++ staff_punch_in.py
@@ -20,7 +20,6 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-
 staff = True
 
 # 使輸入畫面重複出現>
@@ -42,15 +41,18 @@ def staff_input():
         staff_number = raw_input("請輸入員工編號> ")
 
         if staff_number in staff_list:
-            in_or_out = int(raw_input("1，上班。2，下班。請輸入> "))
+            in_or_out = raw_input("1，上班。2，下班。請輸入> ")
+            
             print "\n"
             
-            if in_or_out != 1 and in_or_out != 2:
+            if in_or_out != "1" and in_or_out != "2":
                 print bcolors.WARNING + "資料有誤，請重新輸入。" + bcolors.ENDC
                 raw_input()
                 manual_check_input_data = "n"
                 break
             else:
+                int_in_or_out = int(in_or_out)
+
                 # 產生時間戳記
                 time_in_sec = time.time()
                 time_and_date = datetime.datetime.fromtimestamp(time_in_sec).strftime('%Y/%m/%d, %H:%M:%S')
@@ -59,7 +61,7 @@ def staff_input():
                 print "請確認輸入資料："
                 
                 print "\n"
-                print bcolors.WARNING + staff_list[staff_number], punch_in_or_out[in_or_out], time_and_date + bcolors.ENDC
+                print bcolors.WARNING + staff_list[staff_number], punch_in_or_out[int_in_or_out], time_and_date + bcolors.ENDC
                 print "\n"
         
                 manual_check_input_data = raw_input("確認資料正確？(y/n)")
@@ -85,7 +87,7 @@ def staff_input():
                 target.write(", ")
                 target.write(staff_list[staff_number])
                 target.write(", ")
-                target.write(punch_in_or_out[in_or_out])
+                target.write(punch_in_or_out[int_in_or_out])
                 target.write(", ")
                 target.write(time_and_date)
                 target.write('\n')
